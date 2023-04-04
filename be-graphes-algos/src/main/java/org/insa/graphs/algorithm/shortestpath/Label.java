6c5cd11d33822a8aac2a5daae6b318ef8e95610a
Gautheron Thibault <tgauther@insa-20148>
2023-04-04 16:32:33 +0200

    label

---
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -0,0 +1,39 @@
+package org.insa.graphs.algorithm.shortestpath;
+
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Node;
+
+public class Label {
+    private Node sommet_courant;
+    private boolean marque;
+    private double cout_realise;
+    private Arc pere;
+
+    public Label(Node sommet, boolean marque, double cout, Arc pere){
+        this.sommet_courant = sommet;
+        this.marque = marque;
+        this.cout_realise = cout;
+        this.pere = pere;
+    }
+
+    public Node getSommetCourant(){
+        return this.sommet_courant;
+    }
+
+    public boolean getMarque(){
+        return this.marque;
+    }
+
+    public double getCoutRealise(){
+        return this.cout_realise;
+    }
+
+    public Arc getPere(){
+        return this.pere;
+    }
+
+    public double getCost(){
+        return this.getCoutRealise();
+    }
+
+}
